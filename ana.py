ba30ca8fca7ce834ec98fbc94157ebb4c078c60c
azuldeniz <hallikdeniz@gmail.com>
2022-10-30 14:44:01 +0300

    add remaining files

---
+++ ana.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+
+def patates():
+    print("Hello world!")
+
+    domates = {
+        "1": "1",
+        "2": "2",
+    }
+
+    population_dict = {
+        "California": 38332521,
+        "Texas": 2644,
+        "New York": 19,
+        "Florida": 19552860,
+        "Illinois": 12882135,
+    }
+    population = pd.Series(population_dict)
+    population
+
+
+if __name__ == "__main__":
+    patates()
